282302a1e5b515da314a176065a00b2ff1de1e73
Grade89 <gssdseries2@gmail.com>
2024-06-04 13:13:06 +0200

    Add files via upload

---
+++ Comunidade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Catcare</title>
+  <link rel="shortcut icon" href="Emb2.jpg" >
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<head>
+<body>        
+
+    <div class="footer-copyright">
+      <div class="container">
+
+      <ul class="pagination">
+    <li class="waves-effect"><a href="index.html"><i class="material-icons">chevron_left</i></a></li>
+    <li class="waves-effect"><a href="index.html">1</a></li>
+    <li class="active"><a href="#!">2</a></li>
+    <li class="waves-effect"><a href="contac.html">3</a></li>
+    <li class="waves-effect"><a href="Sobre.html">4</a></li>
+    <li class="waves-effect"><a href="contac.html"><i class="material-icons">chevron_right</i></a></li>
+  </ul>
+      </div>
+
+    </div>
+
+  
+
+
+    <p class="row center"> Made by <a class="Grey-text text-lighten-3" href="#">Edimilson Baltazar Baloi</a></p>
+  </footer>
+
+
+  <!--  Scripts-->
+
+ 
+ <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+       <script src="js/init.js"></script>
+  </body>
+</html>
